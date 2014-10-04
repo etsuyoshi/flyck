ffd3cd51da559ee37ef368db49d31bd7d7c8dddf
tuyo1009 <tuyo1009@gmail.com>
2014-10-04 19:19:30 +0900

    execute

--- flyck/ViewController.m
+++ flyck/ViewController.m
@@ -21,7 +21,8 @@
     UILabel *label;
     UILabel *label2;
     
-    NSMutableArray *arrImage;
+//    NSMutableArray *arrImage;
+    NSMutableArray *arrAssets;
     
     BOOL isFirstDisplay;
 }
@@ -34,7 +35,8 @@
     
     isFirstDisplay = false;
     
-    arrImage = [NSMutableArray array];
+//    arrImage = [NSMutableArray array];
+    arrAssets = [NSMutableArray array];
     
     
     
@@ -105,53 +107,63 @@
             [assets enumerateObjectsUsingBlock:^(PHAsset *asset, NSUInteger idx, BOOL *stop) {
                 NSLog(@"asset:%@", asset);
                 
-//                NSLog(@"arrImage.count = %d", (int)arrImage.count);
+                NSLog(@"arrAssets.count = %d", (int)arrAssets.count);
                 
                 
-                if(arrImage.count < 30){
-                    //4.画像取得
-                    [[PHImageManager defaultManager]
-                     requestImageForAsset:asset
-                     targetSize:self.view.bounds.size//CGSizeMake(300,300)
-                     contentMode:PHImageContentModeAspectFit
-                     options:nil
-                     resultHandler:^(UIImage *result, NSDictionary *info) {
-                         if (result) {
-                             NSLog(@"result %d = %@",(int)arrImage.count, result);
-    //                         _weakSelf.imageView.image = result;
-    //                         _weakSelf.label.text = [NSString stringWithFormat:@"UIImage:%@", NSStringFromCGSize(result.size)];
-    //                         _weakSelf.label2.text = [NSString stringWithFormat:@"imageView:%@", NSStringFromCGSize(self.imageView.frame.size)];
-                             
-                             
-                             [arrImage addObject:result];
-                             
-                             if(arrImage.count > 30){//arrImageが30以上の要素を持つ時
-                                 NSLog(@"arrImageが30以上の要素を持つ");
-                                 if(!isFirstDisplay){//まだ表示してなければ
-                                     label.text = @"大久保君、感想待ってる！";
-                                     label.font = [UIFont systemFontOfSize:17];
-                                     
-                                     isFirstDisplay = true;//表示済ステータスにする
-                                     [self displayimage];
-                                 }
-                                 
-                             }
-                             
-                             
-//                         imageView.image = result;
-//                         label.text =
-//                         [NSString
-//                          stringWithFormat:@"UIImage:%@",
-//                          NSStringFromCGSize(result.size)];
-//                         label2.text =
-//                         [NSString
-//                          stringWithFormat:@"imageView:%@",
-//                          NSStringFromCGSize(imageView.frame.size)];
-//                         
-//                         return ;
-
-                         }
-                     }];
+                if(arrAssets.count < 30){
+                    
+                    [arrAssets addObject:asset];
+                    
+                    if(!isFirstDisplay &&
+                       !(arrAssets.count < 30)){
+                        isFirstDisplay = true;
+                        [self setAssetToMDCSwipe];
+                        
+                        return ;
+                    }
+//                    //4.画像取得
+//                    [[PHImageManager defaultManager]
+//                     requestImageForAsset:asset
+//                     targetSize:self.view.bounds.size//CGSizeMake(300,300)
+//                     contentMode:PHImageContentModeAspectFit
+//                     options:nil
+//                     resultHandler:^(UIImage *result, NSDictionary *info) {
+//                         if (result) {
+//                             NSLog(@"result %d = %@",(int)arrImage.count, result);
+//    //                         _weakSelf.imageView.image = result;
+//    //                         _weakSelf.label.text = [NSString stringWithFormat:@"UIImage:%@", NSStringFromCGSize(result.size)];
+//    //                         _weakSelf.label2.text = [NSString stringWithFormat:@"imageView:%@", NSStringFromCGSize(self.imageView.frame.size)];
+//                             
+//                             
+//                             [arrImage addObject:result];
+//
+//                             if(arrImage.count > 30){//arrImageが30以上の要素を持つ時
+//                                 NSLog(@"arrImageが30以上の要素を持つ");
+//                                 if(!isFirstDisplay){//まだ表示してなければ
+//                                     label.text = @"大久保君、感想待ってる！";
+//                                     label.font = [UIFont systemFontOfSize:17];
+//                                     
+//                                     isFirstDisplay = true;//表示済ステータスにする
+//                                     [self displayimage];
+//                                 }
+//                                 
+//                             }
+//                             
+//                             
+////                         imageView.image = result;
+////                         label.text =
+////                         [NSString
+////                          stringWithFormat:@"UIImage:%@",
+////                          NSStringFromCGSize(result.size)];
+////                         label2.text =
+////                         [NSString
+////                          stringWithFormat:@"imageView:%@",
+////                          NSStringFromCGSize(imageView.frame.size)];
+////                         
+////                         return ;
+//
+//                         }
+//                     }];
                 }
                 
 //                    NSLog(@"arrimage.count = %d", (int)arrImage.count);
@@ -189,9 +201,9 @@
 //    }];
 }
 
--(void)displayimage{
-    NSLog(@"num of factor = %d", (int)arrImage.count);
+-(void)setAssetToMDCSwipe{
     
+    NSLog(@"setAssetToMDCSwipe for asset.count = %d", (int)arrAssets.count);
     
     options = [MDCSwipeToChooseViewOptions new];
     options.delegate = self;
@@ -205,43 +217,91 @@
         }
     };
     
-    
     MDCSwipeToChooseView *viewSwipe;
-    
-    for(int i = 0;i < arrImage.count;i++){
+    PHAsset *asset;
+    for(int i = 0;i < arrAssets.count;i++){
+        NSLog(@"asset %d = %@", i, arrAssets[i]);
         viewSwipe =
         [[MDCSwipeToChooseView alloc]
-         initWithFrame:self.view.bounds//CGRectMake(0, 0, 300, 300)
+         initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width/1.3f,
+                                  self.view.bounds.size.height/1.3f)
          options:options];
         viewSwipe.center = self.view.center;
         viewSwipe.tag = 0;
-//        view.imageView.image = [UIImage imageNamed:@"photo"];
         
-        //画像データ取得
-//        NSString *strUrl =
-//        arrHibitaImageUrl[0];
-        //        arrHibitaImageUrl[i % arrHibitaImageUrl.count][@"img1_500"];
-//        NSLog(@"strUrl = %@", strUrl);
-        
-//        (NSString *)arrTmpImageUrl[i % arrTmpImageUrl.count];
+        asset = (PHAsset *)arrAssets[i];
+        [[PHImageManager defaultManager]
+         requestImageForAsset:asset
+         targetSize:self.view.bounds.size//CGSizeMake(300,300)
+         contentMode:PHImageContentModeAspectFit
+         options:nil
+         resultHandler:^(UIImage *result, NSDictionary *info) {
+             if (result) {
+                 NSLog(@"uiimage of asset = %@", result);
+                 
+                 viewSwipe.imageView.image = result;
+                 viewSwipe.tag = i;
+                 
+                 [self.view addSubview:viewSwipe];
+             }
+         }];
 
-//        NSLog(@"noOfImage = %d : %@", noOfImage, strUrl);
-        
-        viewSwipe.imageView.image = (UIImage *)arrImage[i];
-//        [viewSwipe.imageView
-//         setImageWithURL:[NSURL URLWithString:strUrl]
-//         placeholderImage:nil
-//         completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType){
-//             [viewSwipe.imageView
-//              setImageWithURL:[NSURL URLWithString:strUrl]
-//              placeholderImage:image];
-//             
-//             [SVProgressHUD dismiss];
-//         }];
-        [self.view addSubview:viewSwipe];
     }
-
 }
+
+//-(void)displayimage{
+//    NSLog(@"num of factor = %d", (int)arrImage.count);
+//    
+//    
+//    options = [MDCSwipeToChooseViewOptions new];
+//    options.delegate = self;
+//    options.likedText = @"Like";
+//    options.likedColor = [UIColor blueColor];
+//    options.nopeText = @"Dis";
+//    options.onPan = ^(MDCPanState *state){
+//        if(state.thresholdRatio == 1.f &&
+//           state.direction == MDCSwipeDirectionLeft){
+//            NSLog(@"Let go now to delete the photo!");
+//        }
+//    };
+//    
+//    
+//    MDCSwipeToChooseView *viewSwipe;
+//    
+//    for(int i = 0;i < arrImage.count;i++){
+//        viewSwipe =
+//        [[MDCSwipeToChooseView alloc]
+//         initWithFrame:self.view.bounds//CGRectMake(0, 0, 300, 300)
+//         options:options];
+//        viewSwipe.center = self.view.center;
+//        viewSwipe.tag = 0;
+////        view.imageView.image = [UIImage imageNamed:@"photo"];
+//        
+//        //画像データ取得
+////        NSString *strUrl =
+////        arrHibitaImageUrl[0];
+//        //        arrHibitaImageUrl[i % arrHibitaImageUrl.count][@"img1_500"];
+////        NSLog(@"strUrl = %@", strUrl);
+//        
+////        (NSString *)arrTmpImageUrl[i % arrTmpImageUrl.count];
+//
+////        NSLog(@"noOfImage = %d : %@", noOfImage, strUrl);
+//        
+//        viewSwipe.imageView.image = (UIImage *)arrImage[i];
+////        [viewSwipe.imageView
+////         setImageWithURL:[NSURL URLWithString:strUrl]
+////         placeholderImage:nil
+////         completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType){
+////             [viewSwipe.imageView
+////              setImageWithURL:[NSURL URLWithString:strUrl]
+////              placeholderImage:image];
+////             
+////             [SVProgressHUD dismiss];
+////         }];
+//        [self.view addSubview:viewSwipe];
+//    }
+//
+//}
 
 
 #pragma delegate method of mdcswipe
